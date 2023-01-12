53cd21c59edade8a95951c1a63225d7c99acb241
Rakib <rakib@technovilla.net>
2023-01-13 04:00:25 +0600

    Initial version

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using ConnectionStringTester;
+using Microsoft.EntityFrameworkCore;
+
+try
+{
+    var connectionString = args[0];
+    var context = new AppDbContext(connectionString);
+    var serverName = context.Database.SqlQuery<string>($"SELECT @@SERVERNAME ServerName").ToList().Single();
+    Console.WriteLine($"Got response from sql server '{serverName}'");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
